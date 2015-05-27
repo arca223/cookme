0cc7837edbf014055360cf1227dabad953204daa
arca223 <arcanis223@gmail.com>
2015-05-27 16:49:44 +0200

    validator age ok all input required

--- src/validators/UserAgeValidator.java
+++ src/validators/UserAgeValidator.java
@@ -13,17 +13,22 @@ import javax.faces.validator.ValidatorException;
 
 @FacesValidator(value = "validators.userage")
 public class UserAgeValidator implements Validator {
-	private static final int USERAGE_PATTERN = 100;
+	private static final int AGE_PATTERN = 100;
+	private static final String USERAGE_PATTERN = "[0-9]+";
+	private Pattern pattern;
+	private Matcher matcher;
 
 	public UserAgeValidator() {
+		pattern = Pattern.compile(USERAGE_PATTERN);
 	}
 
 	@Override
 	public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
-		 if(! ((Integer)value<USERAGE_PATTERN)){
+		 matcher = pattern.matcher(value.toString());
+		 if(! (((Integer)value < AGE_PATTERN) && (matcher.matches()))){
 			 FacesMessage msg =
 			 new FacesMessage("user login validation failed.",
-					 "Please respect constraint : inferior to "+USERAGE_PATTERN);
+					 "Please respect constraint : number "+USERAGE_PATTERN+" inferior to "+AGE_PATTERN);
 			 msg.setSeverity(FacesMessage.SEVERITY_ERROR);
 			 throw new ValidatorException(msg); 
 		 }
