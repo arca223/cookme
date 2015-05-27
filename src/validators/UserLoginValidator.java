a117ad2a342ea48ccf344648e528233a76c7a874
arca223 <arcanis223@gmail.com>
2015-05-27 09:43:03 +0200

    validators

---
+++ src/validators/UserLoginValidator.java
@@ -0,0 +1,36 @@
+package validators;
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@FacesValidator(value = "validators.username")
+public class UserLoginValidator implements Validator {
+	private static final String USERLOGIN_PATTERN = "[a-zA-Z0-9-._]";
+	private Pattern pattern;
+	private Matcher matcher;
+
+	public UserLoginValidator() {
+		pattern = Pattern.compile(USERLOGIN_PATTERN);
+	}
+
+	@Override
+	public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+		 matcher = pattern.matcher(value.toString());
+		 if(!matcher.matches()){
+			 FacesMessage msg =
+			 new FacesMessage("user login validation failed.",
+					 "User Login Validation failed please follow the contraint"+USERLOGIN_PATTERN);
+			 msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+			 throw new ValidatorException(msg); 
+		 }
+	 }
+}
+
---
+++ src/validators/UserMailValidator.java
@@ -0,0 +1,36 @@
+package validators;
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@FacesValidator(value = "validators.username")
+public class UserMailValidator implements Validator {
+	private static final String USERMAIL_PATTERN = "[a-zA-Z0-9-._]+@[a-zA-Z0-9-._].[a-z]+";
+	private Pattern pattern;
+	private Matcher matcher;
+
+	public UserMailValidator() {
+		pattern = Pattern.compile(USERMAIL_PATTERN);
+	}
+
+	@Override
+	public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+		 matcher = pattern.matcher(value.toString());
+		 if(!matcher.matches()){
+			 FacesMessage msg =
+			 new FacesMessage("user mail validation failed.",
+					 "User Mail Validation failed please follow the contraint"+USERMAIL_PATTERN);
+			 msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+			 throw new ValidatorException(msg); 
+		 }
+	 }
+}
+
