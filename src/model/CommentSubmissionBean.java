b88c0d578b5135f5e5735370c1928f4526e21401
lbain69 <lblain69@gmail.com>
2015-06-14 16:04:47 +0200

    commentaire

---
+++ src/model/CommentSubmissionBean.java
@@ -0,0 +1,13 @@
+package model;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class CommentSubmissionBean extends CommentModel{
+
+	public CommentSubmissionBean(){
+		super();
+	}
+}
